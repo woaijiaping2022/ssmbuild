6fcc3c70c37472ab7277d45a503459917359a79c
tang <tang@zehua.com>
2022-04-30 18:48:23 +0800

    commit git-ssm 01

--- src/main/java/com/tang/Test/Hello.java
+++ src/main/java/com/tang/Test/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("爱你");
         System.out.println("一生一世");
+        System.out.println("I Love Luna");
     }
 }
